353a84fae52c367f07d0b3f7151fad65decb425a
Katelyn Wilson <kking927@hotmail.com>
2026-02-22 14:41:55 -0600

    Create App component for Tic-Tac-Toe game

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Board from "./components/Board";
+
+function App() {
+  return (
+    <div className="game">
+      <h1>Tic-Tac-Toe</h1>
+      <Board />
+    </div>
+  );
+}
+
+export default App;
